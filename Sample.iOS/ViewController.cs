44b21ba52faec81f4c177b3db33302ab8d1348e8
jgiacomini <archeone@gmail.com>
2017-02-19 23:36:23 +0100

    * Info.plist: Modification de info.plist pour accepter les requête   http non sécurisée
    * ViewController.cs: Ajout de l'exemple ScalesPageToFit

--- Sample.iOS/ViewController.cs
+++ Sample.iOS/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Threading.Tasks;
 using UIKit;
 
 namespace Sample.iOS
@@ -8,14 +8,45 @@ namespace Sample.iOS
     {
         public ViewController(IntPtr handle) : base(handle)
         {
-            UIWebView webView = new UIWebView(View.Bounds);
-            // Ajout du contrôle en tant que sous au contrôleur principal
-            View.AddSubview(webView);
-            // Création de l’uri
-            var uri = new Uri("https://www.google.fr");
-            webView.LoadRequest(new Foundation.NSUrlRequest(uri));
-
+			Tests();
         }
+
+		async void Tests()
+		{
+			UIWebView webView = new UIWebView(View.Bounds);
+			// Ajout du contrôle en tant que sous au contrôleur principal
+			View.AddSubview(webView);
+
+			//await TestUri(webView);
+
+			await TestScalesPageToFitAsync(webView);
+		}
+
+		async Task TestUri(UIWebView webView)
+		{
+			// Création de l’uri
+			var uri = new Uri("http://www.google.fr");
+			webView.LoadRequest(new Foundation.NSUrlRequest(uri));
+
+			await Task.Delay(5000);
+		}
+
+		async Task TestScalesPageToFitAsync(UIWebView webView)
+		{
+			// Url du blog de James Montemagno
+			var uri = new Uri("http://motzcod.es/");
+
+			// par défaut la web view est en ScalesPageToFit à false
+			webView.ScalesPageToFit = false;
+			webView.LoadRequest(new Foundation.NSUrlRequest(uri));
+
+			await Task.Delay(5000);
+
+			// On réaffiche le même site web avec en ScalesPageToFit à true
+			webView.ScalesPageToFit = true;
+			// On recharge la page web
+			webView.LoadRequest(new Foundation.NSUrlRequest(uri));
+		}
 
         public override void ViewDidLoad()
         {
